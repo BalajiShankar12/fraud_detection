3b312e180cc73ef57095dd59950a350b602f91ef
BalajiShankar12 <balajishankar1208@gmail.com>
2025-09-21 21:00:26 +0530

    added 3 new ML model codes

--- ML_model.py
+++ ML_model.py
@@ -1,1 +1,11 @@
 print("Adding a  ML related code here")
+
+def model1():
+    print("Adding code for ML model 1")
+
+def model2():
+    print("Adding code for ML model 1")
+
+def model3():
+    print("Adding code for ML model 1")
+
